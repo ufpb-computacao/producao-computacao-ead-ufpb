d95669e302f6ace0085ae05b1d2a79f6bcd486d2
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@gmail.com>
2013-10-10 11:48:54 -0300

    Inclusão de webhook funcionando

--- sinatra/webhook/webhook.rb
+++ sinatra/webhook/webhook.rb
@@ -1,20 +1,37 @@
 require 'rubygems'
 require 'sinatra'
 require 'logger'
+require 'open3'
+require 'json'
+
+# /usr/bin/curl http://producao.virtual.ufpb.br/cgi-bin/pull-pdf.py?repositorio=https://github.com/edusantana/playground-asciidoc
 
 post '/' do
   push = JSON.parse(params[:payload])
-	# Save a string to a file.
-	myStr = "This is a test"
-	aFile = File.new("myString.txt", "w")
-	aFile.write("I got some JSON: #{push.inspect}")
-	aFile.close
-  logger.info "I got some JSON: #{push.inspect}"
-  "I got some JSON: #{push.inspect}"
+  repo_url = push['repository']['url']
+	File.open('url.txt', 'w') {|f| f.write(push['repository']['url']) }
+
+	Open3.popen3("/usr/bin/curl http://localhost/cgi-bin/pull-pdf.py?repositorio=#{repo_url}") {|stdin, stdout, stderr, wait_thr|
+		pid = wait_thr.pid # pid of the started process.
+		
+		exit_status = wait_thr.value # Process::Status object returned.
+		
+		File.open('log.txt', 'w') {|f| f.write(stdout.read) }
+	}
+
+
 end
 
-get '/' do
-  logger.info 'recebido algo'
-  'xx'
+get '/hi' do
+
+Open3.popen3('/usr/bin/curl http://producao.virtual.ufpb.br/cgi-bin/pull-pdf.py?repositorio=https://github.com/edusantana/playground-asciidoc') {|stdin, stdout, stderr, wait_thr|
+  pid = wait_thr.pid # pid of the started process.
+  
+  exit_status = wait_thr.value # Process::Status object returned.
+  
+  File.open('log.txt', 'w') {|f| f.write(stdout.read) }
+}
+
+  logger.info 'Processamento finalizado.'
 end
 

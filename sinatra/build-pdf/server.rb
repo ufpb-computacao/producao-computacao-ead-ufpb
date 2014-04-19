58f88101f000263e7a9f5268d0c36acda265c475
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@gmail.com>
2014-04-19 18:56:39 -0300

    public folder

--- sinatra/build-pdf/server.rb
+++ sinatra/build-pdf/server.rb
@@ -1,6 +1,8 @@
 require 'sinatra'
 require 'json'
 
+set :public_folder, File.dirname(__FILE__) + '/public'
+
 post '/payload' do
   push = JSON.parse(params[:payload])
   puts "I got some JSON: #{push.inspect}"
